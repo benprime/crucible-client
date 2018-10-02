4ed5963abc5b1bdfa4716daab30bc42b11997e9e
Jeff Smith <jeff@jeffmsmith.com>
2018-10-01 20:45:58 -0700

    Removed the auth portion to just have the react refactor. Added an env file with a port set to 3001 to not conflict with the server default.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import openSocket from 'socket.io-client';
-import { Auth } from 'aws-amplify';
-import Login from './components/Login';
 import Log from './components/Log';
 import CommandInput from './components/CommandInput';
 import Hud from './components/Hud';
@@ -11,37 +9,8 @@ import config from './config';
 class App extends Component {
   socket = openSocket(config.crucibleMudSocketUri);;
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAuthenticated: false,
-      isAuthenticating: true
-    };
-  }
-
   async componentDidMount() {
-    try {
-      if (await Auth.currentSession()) {
-        this.userHasAuthenticated(true);
-      }
-    }
-    catch (e) {
-      if (e !== 'No current user') {
-        alert(e);
-      }
-    }
-    this.setState({ isAuthenticating: false });
     this.focusInput();
-  }
-
-  userHasAuthenticated = authenticated => {
-    this.setState({ isAuthenticated: authenticated });
-  }
-
-  handleLogout = async event => {
-    await Auth.signOut();
-    this.userHasAuthenticated(false);
   }
 
   focusInput() {
@@ -49,17 +18,6 @@ class App extends Component {
   }
 
   render() {
-    const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated
-    };
-    /*
-        var display = <div id="flexbox"><Login props={childProps} /></div>;
-    
-        if (!this.state.isAuthenticating && this.state.isAuthenticated) {
-          display = 
-        }
-    */
     return (
       <div id="app" className="App" onClick={this.focusInput}>
         <Log socket={this.socket} />
--- src/libs/auth.js
+++
@@ -1,1 +0,0 @@
-import { Auth } from "aws-amplify";
